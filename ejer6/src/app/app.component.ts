1ea5943fb94e5fa4703bf1c9b3105e2d152c91ce
scatt89 <dasilvagarciam@gmail.com>
2017-02-14 19:45:34 +0100

    más ejercicios

--- ejer6/src/app/app.component.ts
+++ ejer6/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-		<h1 class="title">Library</h1>
+		<span><md-icon>view_list</md-icon><h1 class="title">Library</h1></span>
     <router-outlet></router-outlet>`,
 })
 export class AppComponent {
